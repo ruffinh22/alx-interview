8e736b1f8d3b0ffb248f2a54713d299794a9b1cf
ruffin hounsounnon <ruffinhounsounnon@gmail.com>
2023-07-06 20:36:05 +0100

    update lock file

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,32 +1,26 @@
 #!/usr/bin/python3
-
-'''
-LockBoxes Challenge
+"""
 Solution to lockboxes problem
-'''
+"""
+
 
 def canUnlockAll(boxes):
-    '''determines if all the boxes can be opened or not
-    Returns:
-        True: all boxes can be opened
-        False: not all boxes can be opened
-    '''
-    # Create a list to keep track of visited boxes
-    visited = [False] * len(boxes)
-    visited[0] = True  # Mark the first box as visited
-    
-    # Create a queue to perform BFS
-    queue = [0]  # Start with the first box
-    
-    # Perform BFS
-    while queue:
-        current_box = queue.pop(0)
-        # Check all the keys in the current box
-        for key in boxes[current_box]:
-            if key >= 0 and key < len(boxes) and not visited[key]:
-                visited[key] = True
-                queue.append(key)
-    
-    # Check if all boxes have been visited
-    return all(visited)
-    
+    """
+    Determines whether a series of locked boxes can be opened
+    based on keys that can be attained.
+    Solution to the lockboxes problem
+    """
+    if (type(boxes)) is not list:
+        return False
+    elif (len(boxes)) == 0:
+        return False
+
+    for k in range(1, len(boxes) - 1):
+        boxes_checked = False
+        for idx in range(len(boxes)):
+            boxes_checked = k in boxes[idx] and k != idx
+            if boxes_checked:
+                break
+        if boxes_checked is False:
+            return boxes_checked
+    return True
